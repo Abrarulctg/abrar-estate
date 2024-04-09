e5a224cd03e32fd3775d08cc9aa40067d8a24954
Abrarulctg <jubairctg99@gmail.com>
2024-04-09 21:18:38 +0600

    Added login and register page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Helmet } from "react-helmet";
+import { NavLink } from 'react-router-dom';
 
 const Login = () => {
+
     return (
         <div>
             <Helmet>
@@ -9,8 +11,39 @@ const Login = () => {
                 <meta name="description" content="Helmet application" />
             </Helmet>
 
-            <h1>Login</h1>
+            <div className="hero">
+                <div className="w-full lg:w-2/5 md:w-3/4 my-10">
+                    <div className="card shrink-0 w-full shadow-2xl bg-base-100">
+                        <h1 className='text-2xl font-bold text-[#002366] mt-4'>Please Login</h1>
+
+                        <form className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" placeholder="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" placeholder="password" className="input input-bordered" required />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+
+                                <input className='btn btn-primary' type="submit" value="Login" />
+                            </div>
+                        </form>
+                        <p>New to Abrar Estate? Please <NavLink to="/register" className="font-[500] text-purple-950">Register</NavLink></p>
+                        <button className='btn btn-ghost'>Signin with Google</button>
+                    </div>
+                </div>
+            </div>
         </div>
+
     );
 };
 
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Helmet } from "react-helmet";
+import { NavLink } from 'react-router-dom';
 
 const Register = () => {
     return (
@@ -8,7 +9,46 @@ const Register = () => {
                 <title>Abrar Estate | Register</title>
                 <meta name="description" content="Helmet application" />
             </Helmet>
-            <h1>Register</h1>
+            <div className="hero">
+                <div className="w-full lg:w-2/5 md:w-3/4 my-10">
+                    <div className="card shrink-0 w-full shadow-2xl bg-base-100">
+                        <h1 className='text-2xl font-bold text-[#002366] mt-4'>Please Register</h1>
+
+                        <form className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" placeholder="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" placeholder="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" placeholder="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" placeholder="password" className="input input-bordered" required />
+
+                            </div>
+                            <div className="form-control mt-6">
+
+                                <input className='btn btn-primary' type="submit" value="Login" />
+                            </div>
+                        </form>
+                        <p>Already have an Account? Please <NavLink to="/login" className="font-[500] text-purple-950">Login</NavLink></p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
