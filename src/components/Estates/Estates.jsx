d846334a8dfe0ff82d3a1b71ef6af6eb34dcdefb
Abrarulctg <jubairctg99@gmail.com>
2024-04-09 13:29:56 +0600

    added router setup and register, contact, about, page

---
+++ src/components/Estates/Estates.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Estates = () => {
+    return (
+        <div>
+            <h1 className="text-3xl">Estates</h1>
+        </div>
+    );
+};
+
+export default Estates;
---
+++ src/components/UpdateProfile/UpdateProfile.jsx
@@ -0,0 +1,11 @@
+
+
+const UpdateProfile = () => {
+    return (
+        <div>
+            <h1 className="text-3xl">Update Profile</h1>
+        </div>
+    );
+};
+
+export default UpdateProfile;
